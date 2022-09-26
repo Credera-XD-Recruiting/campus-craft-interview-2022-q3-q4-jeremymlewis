0fdf122c50ed4a670c4d59ba77a7a55cb61f0af4
jeremymlewis <jeremylewis086@gmail.com>
2022-09-26 10:47:11 -0600

    Place initials in avatar if no src is passed

--- src/components/profile-header.js
+++ src/components/profile-header.js
@@ -4,6 +4,7 @@ export const updateProfileInformation = (data) => {
   const { firstName, lastName, avatarSrc } = data;
   const headerNode = document.querySelector("#profile-header .profile-header");
   const profileAvatarNode = headerNode.querySelector("img");
+  const avatarNode = headerNode.querySelector(".profile-avatar")
   const nameNode = headerNode.querySelector(".profile-info .profile-info-name");
   const underlineNode = headerNode.querySelector(".profile-underline");
 
@@ -22,5 +23,9 @@ export const updateProfileInformation = (data) => {
 
   if (!avatarSrc) {
     profileAvatarNode.remove();
+    let initials = firstName.charAt(0) + lastName.charAt(0);
+    const avatarImg = document.createElement("h1");
+    avatarImg.innerHTML = initials;
+    avatarNode.appendChild(avatarImg);
   }
 };
